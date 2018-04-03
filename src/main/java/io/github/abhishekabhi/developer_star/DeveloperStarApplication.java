b167904722d992758c82f55ccebde5ea5c085e75
abhishek-abhi <abhishekkumarmandal06@gmail.com>
2018-04-03 12:42:59 +0530

    Jquery and maven dependency added into pom

---
+++ src/main/java/io/github/abhishekabhi/developer_star/DeveloperStarApplication.java
@@ -0,0 +1,12 @@
+package io.github.abhishekabhi.developer_star;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloperStarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeveloperStarApplication.class, args);
+	}
+}
